20b03d20b5f2c403d3508b58625d478f13c04cb6
sapan-ostic <sapanagrawal777@gmail.com>
2024-08-20 18:59:49 -0700

    Fix pylint issues

--- ml_lib/utils/kfold.py
+++ ml_lib/utils/kfold.py
@@ -1,6 +1,20 @@
-
+"""
+K-Folds cross-validator
+"""
 
 class KFold:
+    """
+    K-Folds cross-validator
+    """
+
     def __init__(self, n_splits=5, shuffle=False, random_state=None):
-        """ K-Folds cross-validator 
+        """ K-Folds cross-validator
+        """
+
+    def split(self, x, y=None, groups=None):
+        """ Generate indices to split data into training and test set.
+        """
+
+    def get_n_splits(self, x=None, y=None, groups=None):
+        """ Returns the number of splitting iterations in the cross-validator
         """
